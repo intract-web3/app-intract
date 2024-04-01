9086121ad17b4d5251abe817c80ca100eb20058f
Hitesh Gurnani <hiteshgurnani@Hiteshs-MacBook-Pro-2.local>
2024-04-01 16:06:05 +0530

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 import BusinessLanding from '@/components/BusinessLanding/BusinessLanding';
 import { Navbar } from '@/components/Navbar/Navbar';
-import { GlobalFooter } from '@/ui-components/GlobalFooter/GlobalFooter';
 import { Divider } from '@/ui-components/divider/Divider';
 import { BrowserRouter } from 'react-router-dom';
 export default function Home() {
@@ -11,7 +10,6 @@ export default function Home() {
                 <Navbar />
                 <BusinessLanding />
                 <Divider className='my-16 w-full bg-red-300' />
-                <GlobalFooter />
             </BrowserRouter>
         </>
     );
